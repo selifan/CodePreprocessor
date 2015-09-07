3d9ff567c17a2b6e12ef3e7e77de79862df0e4a5
selifan <as-works@narod.ru>
2015-09-07 16:25:45 +0300

    fixed files

--- tests/preproc.php
+++ tests/preproc.php
@@ -9,13 +9,14 @@ $myvars = array(
   'use_auth' => FALSE
   ,'my_auth' => 1
  ,'sw_lang' => 'de'
+ ,'program_title' => 'Sample string to demonstrate %var% substitution in Preprocessor'
 );
 
 $preproc = new CodePreprocessor();
 $preproc->setLF('unix');
 
 echo "Preprocessed text:\n-----------------------------------------\n";
-echo ($preproc -> parse($srcfile, $myvars));
+echo ($preproc -> parse($srcfile, $myvars, TRUE));
 
 echo "\n-----------------------------------------\nError/warning messages:\n";
 echo implode("\n", $preproc->getErrorMessages());
